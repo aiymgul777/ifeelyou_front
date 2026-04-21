41b46f0e41db7d3fe5ccc23601a9436d4cec66bb
arsseen <marat_arsen2020@mail.ru>
2026-04-21 13:42:27 +0500

    hosting commit 2

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1kIs_8Nh6xBJVIJ-vWkbmlN57NHP7r7Y",
-  authDomain: "i-feel-you-cf014.firebaseapp.com",
-  projectId: "i-feel-you-cf014",
-  storageBucket: "i-feel-you-cf014.firebasestorage.app",
-  messagingSenderId: "164783549118",
-  appId: "1:1055783845678:web:1234567890abcdef"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
